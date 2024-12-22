c4c113c89c9fa4ae50aa5b0745c7cde406f2c34b
Nisanth <70921921+Axtro-PhysX@users.noreply.github.com>
2024-12-21 19:40:08 -0500

    add functionality to open & read files

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def main():
+    # Open a file relative to your local environment
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
