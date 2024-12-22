72a3c5d2c5f3317fb9a57f5cdf830147e4610013
Nisanth <70921921+Axtro-PhysX@users.noreply.github.com>
2024-12-22 00:33:30 -0500

    refactor, add count function

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def main():
-    # Open a file relative to your local environment
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    # Open the file
+    text_path = "books/frankenstein.txt"
+    with open(text_path) as f:
+        file_contents = f.read() # Read the file
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+        # Store a count of strings in a variable
+        count = word_count(file_contents)
 
+        # Print the count
+        print(f"{count}")
+    
+def word_count(file):
+    counted = len(file.split())
+    return counted
+    
 main()
