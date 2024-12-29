2ce9185c77486cd0ea8b58deec3a13de10825d6d
Nisanth <70921921+Axtro-PhysX@users.noreply.github.com>
2024-12-28 19:27:00 -0500

    add functionality to count chars, refactor main

--- main.py
+++ main.py
@@ -3,19 +3,35 @@ def word_count(file):
     return counted
 
 def count_chars(file):
-    # We need to add code here
-    pass
+    # Create empty dictionary to store char count
+    lowered = file.lower()
+    char_dict = {}
+
+    # Loop through each char in the file contents
+    for c in lowered:
+        # If char was seen before, increment count by 1
+        if c in char_dict:
+            char_dict[c] += 1
+        # If it's a new character, add it to the dictionary with +1 count
+        else:
+            char_dict[c] = 1    
+    
+    return char_dict
 
 def main():
     # Open the file
     text_path = "books/frankenstein.txt"
-    with open(text_path) as f:
-        file_contents = f.read() # Read the file
+    with open(text_path) as file:
+        file_contents = file.read() # Read the file
 
         # Store a count of strings in a variable
         count = word_count(file_contents)
 
         # Print the count
         print(f"{count} words found in document.")
+
+        # Return character dictionary
+        counted_chars = count_chars(file_contents)
+        print(f"{counted_chars}")
 
 main()
